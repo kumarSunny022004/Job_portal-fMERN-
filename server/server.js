32afb1b4985d68030f311a36a34e7d90bf40496e
KUMARSUNNY <ks9107163@gmail.com>
2025-01-09 23:52:36 +0530

    aaj kuchh nhi kiya

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+// initialize Express
+const app = express()
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/',(req,res) =>res.send("API wroking"))
+
+// port 
+const PORT = process.env.PORT || 5000
+ 
+app.listen(PORT,()=>{
+    console.log(`server is running on the ports ${PORT}`)
+})
